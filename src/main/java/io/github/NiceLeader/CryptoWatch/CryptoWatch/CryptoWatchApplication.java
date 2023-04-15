0a9c976437bf1a082ad316127aa46e550535c3ce
NiceLeader <maciek2002.10@wp.pl>
2023-04-15 20:40:18 +0200

    Initial commit

---
+++ src/main/java/io/github/NiceLeader/CryptoWatch/CryptoWatch/CryptoWatchApplication.java
@@ -0,0 +1,13 @@
+package io.github.NiceLeader.CryptoWatch.CryptoWatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoWatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoWatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/NiceLeader/CryptoWatch/CryptoWatch/CryptoWatchApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.NiceLeader.CryptoWatch.CryptoWatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoWatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
